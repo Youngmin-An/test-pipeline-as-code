11c2e959838e0155e962c8a123cc046041be1383
Youngmin-An <111176950+Youngmin-An@users.noreply.github.com>
2022-09-12 20:55:37 +0900

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+pipeline {
+    agent {
+        kubernetes {
+            containerTemplate{
+                name 'python'
+                image 'python:3.6-slim'
+                command 'sleep'
+                args '99d'
+            }
+        }
+    }
+    stages {
+        stage('Clone') {
+            steps {
+                container('python'){
+                    sh 'ls -al'
+                }
+            }
+        }
+    }
+}
